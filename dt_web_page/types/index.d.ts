18a08341981d8f56de2560a597f4b1a384da9b51
Xavier Méndez Yánez <xmmendezy@gmail.com>
2021-09-21 09:37:54 -0500

    Update

--- dt_web_page/types/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'gulp-clean-css';
-declare module 'gulp-compile-handlebars';
-declare module 'gulp-data';
-declare module 'gulp-sitemap';
-declare module 'handlebars-layouts';
-declare module 'merge2';
